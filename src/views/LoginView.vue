c87d10bd5b6a57cdaf63105dd96e72fb9cde0754
unknown <phattran.2002.work@gmail.com>
2023-03-07 14:49:29 +0700

    add show password feature

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,127 +1,133 @@
 <template>
-    <div class="login">
-        <h1 class="title">Login</h1>
-        <label>
-            <p>Email</p>
-            <input type="email" name="email" placeholder="example@kyanon.digital" autocomplete="on" tabindex="0">
-        </label>
-        <label>
-            <p>Password</p>
-            <input type="password" placeholder="******">
-        </label>
-        <div class="row">
-            <label for="showPassword" class="pw">
-                <input type="checkbox" id="showPassword" name="showPassword" />
-                Show password
-            </label>
-            <button id="submit-btn">Sign In</button>
-        </div>
+  <div class="login">
+    <h1 class="title">Login</h1>
+    <label>
+      <p>Email</p>
+      <input type="email" name="email" placeholder="example@kyanon.digital" autocomplete="on" tabindex="0">
+    </label>
+    <label>
+      <p>Password</p>
+      <input :type='isShowPassword ? "text":"password"' placeholder="******">
+    </label>
+    <div class="row">
+      <label for="showPassword" class="pw">
+        <input type="checkbox" id="showPassword" name="showPassword" v-model="isShowPassword" />
+        Show password
+      </label>
+      <button id="submit-btn">Sign In</button>
     </div>
+  </div>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      isShowPassword: false
+    }
+  }
 }
 </script>
 <style scoped>
 .login {
-    max-width: 400px;
-    background-color: white;
-    padding: 4rem 2rem;
-    margin: 2rem auto;
-    display: block;
-    border-radius: 20px;
-    min-height: 60vh
+  max-width: 400px;
+  background-color: white;
+  padding: 4rem 2rem;
+  margin: 2rem auto;
+  display: block;
+  border-radius: 20px;
+  min-height: 60vh
 }
 
 .login .title {
-    text-align: center;
-    font-family: 'Roboto', sans-serif;
-    font-size: 1.2rem;
-    padding: 2rem;
+  text-align: center;
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.2rem;
+  padding: 2rem;
 }
 
 .login>label>input {
-    display: block;
-    width: 100%;
-    padding: 15px 7px;
-    border-radius: 8px;
-    border: 2px solid #e7e7e9;
-    margin: 0.5rem 0px;
-    margin-bottom: 1rem;
-    -webkit-transition: 0.8s;
-    transition: 0.8s;
-    outline: none;
+  display: block;
+  width: 100%;
+  padding: 15px 7px;
+  border-radius: 8px;
+  border: 2px solid #e7e7e9;
+  margin: 0.5rem 0px;
+  margin-bottom: 1rem;
+  -webkit-transition: 0.8s;
+  transition: 0.8s;
+  outline: none;
 }
 
 @import url('https://fonts.googleapis.com/css2?family=Golos+Text&display=swap');
 
 .login label p {
-    color: #6343e3;
-    font-weight: bold;
-    font-family: 'Golos Text', sans-serif;
+  color: #6343e3;
+  font-weight: bold;
+  font-family: 'Golos Text', sans-serif;
 }
 
 .login label p::after {
-    content: ":";
+  content: ":";
 }
 
 @import url('https://fonts.googleapis.com/css2?family=Quicksand&display=swap');
 
 .login>label>input::placeholder {
-    /* Chrome, Firefox, Opera, Safari 10.1+ */
-    font-family: 'Quicksand', sans-serif;
-    opacity: 1;
-    /* Firefox */
+  /* Chrome, Firefox, Opera, Safari 10.1+ */
+  font-family: 'Quicksand', sans-serif;
+  opacity: 1;
+  /* Firefox */
 }
 
 .login>label>input:-ms-input-placeholder {
-    /* Internet Explorer 10-11 */
-    font-family: 'Quicksand', sans-serif;
+  /* Internet Explorer 10-11 */
+  font-family: 'Quicksand', sans-serif;
 }
 
 .login>label>input::-ms-input-placeholder {
-    /* Microsoft Edge */
-    font-family: 'Quicksand', sans-serif;
+  /* Microsoft Edge */
+  font-family: 'Quicksand', sans-serif;
 }
 
 .login>label>input:focus,
 .login>label>input:focus-visible {
-    border: 2px solid #2e09db;
-    border-radius: 0px;
+  border: 2px solid #2e09db;
+  border-radius: 0px;
 }
 
 .row {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 
 #submit-btn {
-    background: #2e09db;
-    border: none;
-    color: whitesmoke;
-    padding: 0.6rem 1.5rem;
-    border-radius: 5px;
-    cursor: pointer;
-    -webkit-transition: .5s;
-    transition: .5s;
+  background: #2e09db;
+  border: none;
+  color: whitesmoke;
+  padding: 0.6rem 1.5rem;
+  border-radius: 5px;
+  cursor: pointer;
+  -webkit-transition: .5s;
+  transition: .5s;
 }
 
 @import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
 
 .pw {
-    cursor: pointer;
-    font-family: 'Golos Text', sans-serif;
-    letter-spacing: .1rem;
+  cursor: pointer;
+  font-family: 'Golos Text', sans-serif;
+  letter-spacing: .1rem;
 }
 
 #showPassword {
-    height: 15px;
-    width: 15px;
+  height: 15px;
+  width: 15px;
 }
-#submit-btn:hover{
-    background-color: #4BB543;
-    border-radius: 3px;
-    transform: scale(1.02);
+
+#submit-btn:hover {
+  background-color: #4BB543;
+  border-radius: 3px;
+  transform: scale(1.02);
 }
 </style>
